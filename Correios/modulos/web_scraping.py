f44eb003413cb26fb5ee8466df1b6224f72e2aa5
SANTOSTAVARES <gsantostavares@gmail.com>
2022-04-17 23:33:28 -0300

    Este commit está com o código que que é possível clicar no campo de busca do Google.

--- Correios/modulos/web_scraping.py
+++ Correios/modulos/web_scraping.py
@@ -1,14 +1,10 @@
+from lib2to3.pgen2 import driver
 from selenium import webdriver
-import selenium
-""" 
-navegador = webdriver.Chrome()
-navegador.get("https://www2.correios.com.br/sistemas/buscacep/BuscaCepEndereco.cfm")
-navegador.find_element_by_name('//*[@id="Geral"]/div/div/span[2]/label/input').send_keys('38400322') 
- """
+from selenium.webdriver.common.by import By
+
 
 navegador = webdriver.Chrome()
 navegador.get("https://www.google.com.br")
-navegador.find_element_by_name('q').send_keys("gabriel tavares")
-#navegador.find_element_by_name('btnK').click()
-navegador.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]')
-
+navegador.find_element_by_name('q').send_keys("botafogo")
+button = navegador.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]')
+button.click()
