1666709faf4af76e04f701f1f72170b19cbf0d3d
SANTOSTAVARES <gsantostavares@gmail.com>
2022-04-18 22:49:32 -0300

    foi adicionado o código que obtem o html da página.

--- Correios/modulos/web_scraping.py
+++ Correios/modulos/web_scraping.py
@@ -2,9 +2,10 @@ from lib2to3.pgen2 import driver
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
 navegador = webdriver.Chrome()
-navegador.get("https://www.google.com.br")
-navegador.find_element_by_name('q').send_keys("botafogo")
-button = navegador.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]')
+navegador.get("https://www2.correios.com.br/sistemas/buscacep/BuscaCepEndereco.cfm")
+navegador.find_element_by_name('relaxation').send_keys("38400")
+button = navegador.find_element(By.XPATH, '//*[@id="Geral"]/div/div/div[6]/input')
 button.click()
+html = navegador.page_source
+print(html)
