394d4d98de5386f35e38221eb2fd4648a7cf18cc
SANTOSTAVARES <gsantostavares@gmail.com>
2022-04-20 19:20:49 -0300

    as funções csv_to_dict() e buscar_endereço_correios() estão prontas.

--- Correios/modulos/web_scraping.py
+++ Correios/modulos/web_scraping.py
@@ -1,59 +1,59 @@
-from cgitb import html
 from lib2to3.pgen2 import driver
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from html.parser import HTMLParser
 import re
-"""
-navegador = webdriver.Chrome()
-navegador.get("https://www2.correios.com.br/sistemas/buscacep/BuscaCepEndereco.cfm")
-navegador.find_element_by_name('relaxation').send_keys("38400")
-button = navegador.find_element(By.XPATH, '//*[@id="Geral"]/div/div/div[6]/input')
-button.click()
-ab = navegador.find_element_by_tag_name('tbody')
-a1 = ab.text
-print(re.split('\n|  ', a1))
+import csv
 
-""" 
-print('começoooooou')
-url = "https://www2.correios.com.br/sistemas/buscacep/BuscaCepEndereco.cfm"
-abrir_browser = webdriver.Chrome()
-abrir_browser.get(url)
-dado_para_buscar = '38400322'
-abrir_browser.find_element_by_name('relaxation').send_keys("38400")
-xpath = '//*[@id="Geral"]/div/div/div[6]/input'
-botao_pesquisar = abrir_browser.find_element(By.XPATH, xpath)
-clicar_botao = botao_pesquisar.click()
-tabela_endereco_browser = abrir_browser.find_element_by_tag_name('tbody').text
-lista_dados_endereco = re.split('\n|  ', tabela_endereco_browser)
-lista_dados_endereco.pop(0)
-print(lista_dados_endereco[0])
+pagina_web = 'link_caminho.csv'
+def csv_to_dict(arquivo):
+    
+    with open(arquivo) as arquivo_csv:
+        dados_csv = csv.reader(arquivo_csv, delimiter=',')
+        linha_csv = 1
+        for x in dados_csv:
 
-tabela_endereco = []
-linha_tabela = ['', '', '', '']
-coluna = 0
-for i in lista_enderecos:
-    if coluna == 1 and  i == '':
-        continue
-    elif coluna == 2 and linha_tabela[1] == '':
-        linha_tabela[1] = i
-        continue
-    elif coluna == 3:
-        linha_tabela[]
-    else:
-        coluna += 1
-    linha_tabela[coluna] = i
+            if linha_csv == 1:
+                chave = x           
+            else:
+                valor = x
+            for y in x:            
+                pass
+            linha_csv += 1
 
-print('finalizooooou')    
-#html1 = navegador.page_source
-#print(type(navegador))
-#print(type(html1))
-#HTMLParser.handle_starttag(html, <head>, <body>)
-#a = html1.find('<a>')
-#a = html1.find('div')
-#b = html1
-#print(html1[6600: 7000])
-#b = html1.find('div')
-#print(b)
+    return dict(zip(chave, valor))
 
+#print(csv_to_dict(pagina_web)['url'])
 
+def buscar_endereco_correios(endereco):
+
+    url = csv_to_dict(pagina_web)['url']
+    abrir_browser = webdriver.Chrome()
+    abrir_browser.get(url)
+    
+    abrir_browser.find_element_by_name('relaxation').send_keys(endereco)
+    xpath = csv_to_dict(pagina_web)['xpath']
+    botao_pesquisar = abrir_browser.find_element(By.XPATH, xpath)
+    clicar_botao = botao_pesquisar.click()
+    tabela_endereco_browser = abrir_browser.find_element_by_tag_name('tbody').text
+
+    lista_dados_endereco = re.split('\n|  ', tabela_endereco_browser)
+    lista_dados_endereco.pop(0)
+    tabela_endereco = []
+    linha_tabela = []
+    penultimo_dado_vazio = False
+
+    for i in lista_dados_endereco:
+        if i != '':
+            if penultimo_dado_vazio == True:
+                linha_tabela[-1] = linha_tabela[-1] + ' ' + i
+                penultimo_dado_vazio = False
+            else:
+                linha_tabela.append(i)
+                if len(linha_tabela) == 4:
+                    tabela_endereco.append(linha_tabela)
+                    linha_tabela = []
+        else:
+            penultimo_dado_vazio = True
+    return tabela_endereco
+
+print(buscar_endereco_correios('38400'))  
