84d0a9202a022bd60d299b302540b0d6e851a9f8
SANTOSTAVARES <gsantostavares@gmail.com>
2022-04-24 23:59:14 -0300

    todo o web_scraping está feito. falta adicionar a iteração na planilha de entrada.

--- Correios/modulos/web_scraping.py
+++ Correios/modulos/web_scraping.py
@@ -22,76 +22,35 @@ def csv_to_dict(arquivo):
 
     return dict(zip(chave, valor))
 
-#print(csv_to_dict(pagina_web)['url'])
+def iterar_planilha_endereco():
+    return 'feliz natal'
 
-def buscar_endereco_correios(endereco):
+#print(iterar_planilha_endereco())
 
-    url = csv_to_dict(pagina_web)['url']
-    abrir_browser = webdriver.Chrome()
-    abrir_browser.get(url)
-    
-    abrir_browser.find_element_by_name('relaxation').send_keys(endereco)
-    xpath = csv_to_dict(pagina_web)['xpath']
-    botao_pesquisar = abrir_browser.find_element(By.XPATH, xpath)
-    clicar_botao = botao_pesquisar.click()
-    tabela_endereco_browser = abrir_browser.find_element_by_tag_name('tbody').text
-       
-    #******************** dividir em duas funções *******************
-
-    lista_dados_endereco = re.split('\n|  ', tabela_endereco_browser)
-    lista_dados_endereco.pop(0)
-    tabela_endereco = []
-    linha_tabela = []
-    penultimo_dado_vazio = False
-
-    for i in lista_dados_endereco:
-        if i != '':
-            if penultimo_dado_vazio == True:
-                linha_tabela[-1] = linha_tabela[-1] + ' ' + i
-                penultimo_dado_vazio = False
-            else:
-                linha_tabela.append(i)
-                if len(linha_tabela) == 4:
-                    tabela_endereco.append(linha_tabela)
-                    linha_tabela = []
-        else:
-            penultimo_dado_vazio = True
-    return tabela_endereco
-
-palavra = '1 a 10 de 1'
-def qtde_resultado_pag1(resultado_retornado):
+def qtde_resultado_pag1():
     "Retorna o número que está entre a letra 'a' e a palavra 'de' no texto."
     
-    posicao_a = resultado_retornado.find('a')
-    posicao_de = resultado_retornado.find('de')   
-    return int(palavra[posicao_a + 2:posicao_de - 1])
+    xpath_qtde_resultado = csv_to_dict(pagina_web)['quantidade_resultado']
+    texto_resultado = abrir_browser.find_elements_by_xpath(xpath_qtde_resultado)
+    for i in texto_resultado:
+        texto = i.text
 
+    msg_dados_encontrados = texto[0:30]
+    msg_qtde_resultado = texto[73:]
+    posicao_a = msg_qtde_resultado.find('a')
+    posicao_de = msg_qtde_resultado.find('de')
+    msg_qtde_resultado = int(msg_qtde_resultado[posicao_a + 2:posicao_de - 1])
+    
+    return(msg_dados_encontrados, msg_qtde_resultado)
+    
+def resultado_pesquisa():
 
-def iterar_planilha_endereco():
-    return '38400322'
-
-def acessar_site(site):
     url = csv_to_dict(pagina_web)['url']
+    global abrir_browser
     abrir_browser = webdriver.Chrome()
     abrir_browser.get(url)
-    a2 = iterar_planilha_endereco()
-    site(abrir_browser, a2)
-
-@acessar_site
-def pesquisar_endereco(abrir_browser, endereco):
-    abrir_browser.find_element_by_name('relaxation').send_keys(endereco)
-    xpath = csv_to_dict(pagina_web)['xpath']
-    botao_pesquisar = abrir_browser.find_element(By.XPATH, xpath)
-    clicar_botao = botao_pesquisar.click()
-    tabela_endereco_browser = abrir_browser.find_element_by_tag_name('tbody').text
-
-def teste1():
-    endereco = 'feliz natal'
-    url = csv_to_dict(pagina_web)['url']
-    abrir_browser = webdriver.Chrome()
-    abrir_browser.get(url)
-
-    abrir_browser.find_element_by_name('relaxation').send_keys(endereco)
+    texto_para_buscar = iterar_planilha_endereco()
+    abrir_browser.find_element_by_name('relaxation').send_keys(texto_para_buscar)
     xpath = csv_to_dict(pagina_web)['xpath']
     botao_pesquisar = abrir_browser.find_element(By.XPATH, xpath)
     clicar_botao = botao_pesquisar.click()
@@ -104,7 +63,7 @@ def teste1():
     linha = 1
     coluna = 1
 
-    for x in range(qtde_resultado_pag1(palavra)):
+    for x in range(qtde_resultado_pag1()[1]):
         linha += 1
         for y in range(4):
             celula = abrir_browser.find_elements_by_xpath(xpath_celula_parte1 + str(linha) + xpath_celula_parte2 + str(coluna) + xpath_celula_parte3)
@@ -117,4 +76,42 @@ def teste1():
     
     return tabela_endereco
 
-print(teste1())
+#print(resultado_pesquisa())
+#print(csv_to_dict(pagina_web)['quantidade_resultado'])
+
+def a1():
+    url = csv_to_dict(pagina_web)['url']
+    global abrir_browser
+    abrir_browser = webdriver.Chrome()
+    abrir_browser.get(url)
+    texto_para_buscar = iterar_planilha_endereco()
+    abrir_browser.find_element_by_name('relaxation').send_keys(texto_para_buscar)
+    xpath = csv_to_dict(pagina_web)['xpath']
+    botao_pesquisar = abrir_browser.find_element(By.XPATH, xpath)
+    clicar_botao = botao_pesquisar.click()
+
+def a2():
+    a1()
+    xpath_celula_parte1 = csv_to_dict(pagina_web)['xpath_celula_1']
+    xpath_celula_parte2 = csv_to_dict(pagina_web)['xpath_celula_2']
+    xpath_celula_parte3 = csv_to_dict(pagina_web)['xpath_celula_3']
+    tabela_endereco = []
+    colunas_por_linha = []
+    linha = 1
+    coluna = 1
+
+    for x in range(qtde_resultado_pag1()[1]):
+        linha += 1
+        for y in range(4):
+            celula = abrir_browser.find_elements_by_xpath(xpath_celula_parte1 + str(linha) + xpath_celula_parte2 + str(coluna) + xpath_celula_parte3)
+            for z in celula:
+                colunas_por_linha.append(z.text)
+            coluna += 1
+        tabela_endereco.append(colunas_por_linha)
+        colunas_por_linha = []
+        coluna = 1
+    abrir_browser.close()
+    
+    return tabela_endereco
+
+print(a2())
